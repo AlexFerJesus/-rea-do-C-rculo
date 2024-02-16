0c93663ca04462d5744794160fc1891dd743e657
AlexFerJesus <141464221+AlexFerJesus@users.noreply.github.com>
2024-02-16 00:58:06 +0000

    SALVAR

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Faça o calculo da área");
+
+Console.Write("A =");
+Console.Write(" N(3,14)");
+Console.WriteLine(" . raio²");
+
+Console.WriteLine("coloque o valor do raio para ser multiplicado por N");
+double raio = double.Parse (Console.ReadLine());
+double r = Math.Pow(raio,2);
+
+double PI = 3.14159;
+
+double mult = r * PI;
+
+Console.WriteLine($"A area do circulo é: {mult}");
